1a1d282b4a9b3863878d2b14654331bd52da976f
cmilk18 <junhee1883@naver.com>
2023-02-08 14:12:16 +0900

    기본 설정

---
+++ src/test/java/com/midsangam/utx/UtxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.midsangam.utx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UtxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
